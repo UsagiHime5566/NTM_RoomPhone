80434fca94f8a514da25bfcb4e25ebdd75d52c02
nikki <moniqshiin@gmail.com>
2022-02-14 22:47:35 +0800

    移動TMP, 增加存檔外掛

--- Assets/Scripts/Nodes/NodeDeclaration.cs
+++ Assets/Scripts/Nodes/NodeDeclaration.cs
@@ -2,14 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using I2.Loc;
 
 public class NodeDeclaration : NodeControlBase
 {
     public Button BTN_Disagree;
+    public Button BTN_zhTW;
+    public Button BTN_Japan;
     void Start()
     {
         BTN_Disagree.onClick.AddListener(() => {
             Application.Quit();
+        });
+
+        BTN_zhTW.onClick.AddListener(() => {
+            LocalizationManager.CurrentLanguage = "Chinese (Traditional)";
+        });
+
+        BTN_Japan.onClick.AddListener(() => {
+            LocalizationManager.CurrentLanguage = "Japanese";
         });
     }
 
--- Assets/Scripts/Nodes/NodeHome.cs
+++ Assets/Scripts/Nodes/NodeHome.cs
@@ -3,22 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Video;
 using UnityEngine.UI;
-using I2.Loc;
+
 
 public class NodeHome : NodeControlBase
 {
     public VideoPlayer backgroundPlayer;
-    public Button BTN_zhTW;
-    public Button BTN_Japan;
+    public Button EnterGame;
     void Start()
     {
-        BTN_zhTW.onClick.AddListener(() => {
-            LocalizationManager.CurrentLanguage = "Chinese (Traditional)";
-        });
-
-        BTN_Japan.onClick.AddListener(() => {
-            LocalizationManager.CurrentLanguage = "Japanese";
-        });
+        
     }
 
     public override void OnShowTodo(){
