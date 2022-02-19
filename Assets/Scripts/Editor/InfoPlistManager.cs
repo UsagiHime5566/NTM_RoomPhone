c2da00e81a5d525ee2af6e4940144ccf83f1314c
nikki <cuapopomama2@gmail.com>
2022-02-20 04:21:09 +0800

    更新 iOS Build 設置

--- Assets/Scripts/Editor/InfoPlistManager.cs
+++ Assets/Scripts/Editor/InfoPlistManager.cs
@@ -23,9 +23,10 @@ public class InfoPlistManager : MonoBehaviour
         PlistElementDict rootDict = plist.root;
         rootDict.SetString("NSPhotoLibraryUsageDescription", "開啟後，此APP可以將目前的畫面保存到你的相簿之中");
 	    rootDict.SetString("NSPhotoLibraryAddUsageDescription", "開啟後，此APP可以將目前的畫面保存到你的相簿之中");
-        rootDict.SetString("NSLocationAlwaysAndWhenInUseUsageDescription", "開啟後，APP中的地圖將定位您的位置，並引導您至此APP中的AR觀看點");
-        rootDict.SetString("NSLocationWhenInUseUsageDescription", "開啟後，APP中的地圖將定位您的位置，並引導您至此APP中的AR觀看點");
-        rootDict.SetString("NSCameraUsageDescription", "開啟後，您才能於APP中使用相機來觀看擴增實境(AR)內容");
+        //rootDict.SetString("NSLocationAlwaysAndWhenInUseUsageDescription", "開啟後，APP中的地圖將定位您的位置，並引導您至此APP中的AR觀看點");
+        //rootDict.SetString("NSLocationWhenInUseUsageDescription", "開啟後，APP中的地圖將定位您的位置，並引導您至此APP中的AR觀看點");
+        rootDict.SetString("NSCameraUsageDescription", "開啟後，您才能於APP中正常使用相機錄製影片。");
+        rootDict.SetString("NSMicrophoneUsageDescription", "開啟後，能夠在APP中同時錄製聲音。");
         
         var rootDicVal = rootDict.values;
         rootDicVal.Remove("UIApplicationExitsOnSuspend");
